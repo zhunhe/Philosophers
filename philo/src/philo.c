bd8bda4020cb8a9fb1d2007e54397a4582d7f5d6
zhunhe <xuzhunhe@gmail.com>
2022-04-17 23:56:17 +0900

    fix: Change take_fork()

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 15:32:05 by juhur             #+#    #+#             */
-/*   Updated: 2022/04/16 17:53:43 by juhur            ###   ########.fr       */
+/*   Updated: 2022/04/17 23:55:57 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,16 @@
 
 static void	take_fork(t_philo *p)
 {
-	if (p->order & 1)
-	{
-		pthread_mutex_lock(p->left_fork);
-		print_log(p, TAKEN_A_FORK);
-		pthread_mutex_lock(p->right_fork);
-		print_log(p, TAKEN_A_FORK);
-	}
-	else
-	{
-		pthread_mutex_lock(p->right_fork);
-		print_log(p, TAKEN_A_FORK);
-		pthread_mutex_lock(p->left_fork);
-		print_log(p, TAKEN_A_FORK);
-	}
+	pthread_mutex_lock(p->left_fork);
+	print_log(p, TAKEN_A_FORK);
+	pthread_mutex_lock(p->right_fork);
+	print_log(p, TAKEN_A_FORK);
 }
 
 void	put_down_fork(t_philo *p)
 {
-	if (p->order & 1)
-	{
-		pthread_mutex_unlock(p->left_fork);
-		pthread_mutex_unlock(p->right_fork);
-	}
-	else
-	{
-		pthread_mutex_unlock(p->right_fork);
-		pthread_mutex_unlock(p->left_fork);
-	}
+	pthread_mutex_unlock(p->left_fork);
+	pthread_mutex_unlock(p->right_fork);
 }
 
 static void	eating(t_philo *p)
